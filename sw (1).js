e9a3ea08c4fadcb3c4725722332731b619eb3d11
laguirrevet <lushoa720@gmail.com>
2026-06-30 21:40:06 -0400

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,21 @@
+const CACHE = 'equinutricion-v1';
+const ASSETS = [
+  '/equinutricion/',
+  '/equinutricion/index.html',
+  '/equinutricion/manifest.json',
+  '/equinutricion/icon-192.png',
+  '/equinutricion/icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
